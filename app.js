1d02c8447d7c0d42a014ab6cf96db9995e6ca445
Nadine101 <nadinet026@gmail.com>
2022-07-13 23:40:31 -0600

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
-function multiply (numeral_bay, numeral_bye){
-    let numeral_bay = numeral_bye.multiply();
-    let (numeral_bye) , (numeral_bay)={};
+//here I use integers and add 2 arugument with the if statment to return the the answer//
+function add(n1=4,n2=1){
+   if(n1+n2)
+   return 4+1*5
+
+   //I then use the build in mulitiplcation function and cosole.log the correction and answer//
+   function multipl(n1,n2) {
+    return 5*5
+   }
+   console.log("25 if this is the correct answer");
+}
+
+//here I use the retun statement in this argument to return a ture of false display//
+function user_name (Andrew){
+    if ('the characters in the name Andrew is >10'){
+    return true;
+   }  else {
+    console.log('false')
+   }
+}
+
+//I got stuck on this last one as i got a bit confuse the elememt and the starts
+//with "ph"  and how to call the functions 3 times.
+ //ever time i try to put and argument i keep geting an console error and when 
+ //I follow the the suggestion that its gives when i hover over it, it is still come out wrong//
+ //and my debugger stop working//
+ 
+ function element (ph)
+["phonics","pharmcy","pharmacutical"]
+{
+    return startswith [ph]
 }
